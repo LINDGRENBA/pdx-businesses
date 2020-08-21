ce72c3ae4399f26a5e6c0063fb1bd55f67643151
Brittany Lindgren <brittdevelopsit@gmail.com>
2020-08-21 15:37:55 -0700

    Add using statements and namespace to response.cs.

--- PdxBusiness/Wrappers/Response.cs
+++ PdxBusiness/Wrappers/Response.cs
@@ -1,19 +1,28 @@
-public class Response<T>
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PdxBusiness.Wrappers
 {
-  public Response()
+  public class Response<T>
   {
+    public Response()
+    {
 
-  }
+    }
 
-  public Response(T data)
-  {
-    Succeeded = true;
-    Message = string.Empty;
-    Errors = null;
-    Data = data;
+    public Response(T data)
+    {
+      Succeeded = true;
+      Message = string.Empty;
+      Errors = null;
+      Data = data;
+    }
+    public T Data { get; set; }
+    public bool Succeeded { get; set; }
+    public string[] Errors { get; set; }
+    public string Message { get; set; }
   }
-  public T Data { get; set; }
-  public bool Succeeded { get; set; }
-  public string[] Errors { get; set; }
-  public string Message { get; set; }
 }
+
+
+
